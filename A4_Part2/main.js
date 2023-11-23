57c7160aa1e532024223ef6a3f24efaa7e56db0f
maxwelldinsmore1 <145155020+maxwelldinsmore1@users.noreply.github.com>
2023-11-23 11:08:13 -0500

    Working on A4p2 got darken to work

--- A4_Part2/main.js
+++ A4_Part2/main.js
@@ -5,17 +5,27 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+const imageArray[0] = ["pic1.jpg", "pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.jpg"]
 
 /* Declaring the alternative text for each image file */
 
 /* Looping through images */
-
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
-
+for (images in imageArray) {
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', xxx);
+    newImage.setAttribute('alt', xxx);
+    thumbBar.appendChild(newImage);
+}
 /* Wiring up the Darken/Lighten button */
-
-
-// DARKEN https://www.geeksforgeeks.org/how-to-darken-an-image-using-css/
+btn.addEventListener('click', () => {
+    let styleSet = btn.getAttribute("class")
+    if (styleSet === "dark") {
+        btn.textContent = "Lighten";
+        btn.setAttribute("class", "light");
+        overlay.style.backgroundColor =  "rgba(0,0,0,0.5)";
+    } else {
+        btn.setAttribute("class", "dark");
+        btn.textContent = "Darken";
+        overlay.style.backgroundColor =  "rgba(0,0,0,0)";
+    }
+});
